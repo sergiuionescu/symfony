d48ea931c53d4a1ec60f0c05c332e639022c8515
Sergiu Ionescu <sergiu.ionescu@gmail.com>
2016-03-12 16:24:59 +0200

    Adding support metadata

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,8 @@ license          'Apache 2.0'
 description      'Installs/Configures symfony'
 long_description 'Installs/Configures symfony'
 version          '0.1.2'
+source_url       'https://github.com/sergiuionescu/symfony' if respond_to?(:source_url)
+issues_url       'https://github.com/sergiuionescu/lamp/symfony' if respond_to?(:issues_url)
+
 
 depends 'lamp'
